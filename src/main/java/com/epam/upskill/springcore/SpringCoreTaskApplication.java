cc6bf53c973bc5d83658bf7a44130c6129b47672
Ikromov.Oybek <oybek.ikromov@ssd.uz>
2023-11-08 04:24:48 +0500

    start

---
+++ src/main/java/com/epam/upskill/springcore/SpringCoreTaskApplication.java
@@ -0,0 +1,13 @@
+package com.epam.upskill.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/upskill/springcore/SpringCoreTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.upskill.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
